8b0d6e0397fe2230fd95ff0966593ed89226d986
timcampbell <tim.campbell127@gmail.com>
2016-07-01 04:26:34 -0400

    testing

---
+++ src/main/java/com/sopa89/TestingMod/TestingMod.java
@@ -0,0 +1,11 @@
+package main.java.com.sopa89.TestingMod;
+
+import main.java.com.sopa89.TestingMod.reference.Reference;
+
+import net.minecraftforge.fml.common.Mod;
+
+public class TestingMod
+{
+	@Mod.Instance(Reference.MOD_ID)
+	public static TestingMod instance;
+}
---
+++ src/main/java/com/sopa89/TestingMod/reference/Reference.java
@@ -0,0 +1,8 @@
+package main.java.com.sopa89.TestingMod.reference;
+
+public class Reference
+{
+	public static final String MOD_ID="TestingMod";
+	public static final String MOD_NAME="Testing Mod";
+	public static final String VERSION="1.9.4-1.0";
+}
