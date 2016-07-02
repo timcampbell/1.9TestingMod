e420ff7baee21616b6a044899ca0d7d19cc65033
timcampbell <tim.campbell127@gmail.com>
2016-07-02 15:46:15 -0400

    Created sandwich class

---
+++ src/main/java/com/sopa89/testingmod/item/ItemFoodSandwich.java
@@ -0,0 +1,15 @@
+package com.sopa89.testingmod.item;
+
+import com.sopa89.testingmod.item.base.ItemFoodTM;
+import com.sopa89.testingmod.reference.Names;
+
+public class ItemFoodSandwich extends ItemFoodTM
+{
+
+	public ItemFoodSandwich()
+	{
+		super(13, 14F);
+		this.setUnlocalizedName(Names.Items.SANDWICH);
+	}
+
+}
