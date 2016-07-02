43e6bdb09fef3b7e3341769f559294dc53de0713
timcampbell <tim.campbell127@gmail.com>
2016-07-02 15:48:24 -0400

    registered sandwich item and recipes for sandwich,  added cooked foods to ore dictionary

--- src/main/java/com/sopa89/testingmod/init/ModItems.java
+++ src/main/java/com/sopa89/testingmod/init/ModItems.java
@@ -1,12 +1,19 @@
 package com.sopa89.testingmod.init;
 
+import java.util.ArrayList;
+
 import com.sopa89.testingmod.item.ItemChain;
-import com.sopa89.testingmod.item.ItemTM;
+import com.sopa89.testingmod.item.ItemFoodSandwich;
+import com.sopa89.testingmod.item.base.ItemFoodTM;
+import com.sopa89.testingmod.item.base.ItemTM;
 import com.sopa89.testingmod.reference.Names;
 import com.sopa89.testingmod.reference.Reference;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
@@ -15,19 +22,35 @@ import net.minecraftforge.oredict.OreDictionary;
 
 public class ModItems 
 {
-	public static final String INV="inventory";
 	
 	public static final ItemTM chain=new ItemChain();
+	public static final ItemFoodTM sandwich=new ItemFoodSandwich();
 	
 	public static void init()
 	{
 		GameRegistry.register(chain, new ResourceLocation(Reference.MOD_PREFIX+Names.Items.CHAIN));
+		GameRegistry.register(sandwich, new ResourceLocation(Reference.MOD_PREFIX+Names.Items.SANDWICH));
+		
 		OreDictionary.registerOre("chainIron", new ItemStack(chain));
+		
+		OreDictionary.registerOre("bread", Items.BREAD);
+		
+		OreDictionary.registerOre("meatCooked", Items.COOKED_PORKCHOP);
+		OreDictionary.registerOre("meatCooked", Items.COOKED_BEEF);
+		OreDictionary.registerOre("meatCooked", Items.COOKED_CHICKEN);
+		OreDictionary.registerOre("meatCooked", Items.COOKED_MUTTON);
+		OreDictionary.registerOre("meatCooked", Items.COOKED_RABBIT);
+		OreDictionary.registerOre("meatCooked", new ItemStack(Items.COOKED_FISH, 1, 0));
+		OreDictionary.registerOre("meatCooked", new ItemStack(Items.COOKED_FISH, 1, 1));
+		
+		OreDictionary.registerOre("barIron", Blocks.IRON_BARS);
+		
 	}	
 	
 	public static void reg()
 	{
 		regHelper(Reference.MOD_PREFIX+Names.Items.CHAIN, chain);
+		regHelper(Reference.MOD_PREFIX+Names.Items.SANDWICH, sandwich);
 	}
 	
 	private static void regHelper(String name, Item item)
--- src/main/java/com/sopa89/testingmod/init/Recipes.java
+++ src/main/java/com/sopa89/testingmod/init/Recipes.java
@@ -12,10 +12,13 @@ public class Recipes
 {
 	public static void init()
 	{
-		GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(ModItems.chain, 2), new ItemStack(Blocks.IRON_BARS), new ItemStack(Blocks.IRON_BARS), new ItemStack(Blocks.IRON_BARS), new ItemStack(Blocks.IRON_BARS)));
+		GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(ModItems.chain, 2), "barsIron", "barsIron", "barsIron", "barsIron"));
+		
 		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(Items.CHAINMAIL_BOOTS), "c c", "c c", 'c', "chainIron"));
 		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(Items.CHAINMAIL_LEGGINGS), "ccc", "c c", "c c", 'c', "chainIron"));
 		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(Items.CHAINMAIL_CHESTPLATE), "c c", "ccc", "ccc", 'c', "chainIron"));
 		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(Items.CHAINMAIL_HELMET), "ccc", "c c", 'c', "chainIron"));
+		
+		GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(ModItems.sandwich), "meatCooked", "bread"));
 	}
 }
