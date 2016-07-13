146a3d2e50612502369e10c985f746365ca5396a
timcampbell <tim.campbell127@gmail.com>
2016-07-13 01:33:03 -0400

    Created model, texture, and class for the gold tag item

---
+++ src/main/java/com/sopa89/testingmod/item/ItemGoldTag.java
@@ -0,0 +1,13 @@
+package com.sopa89.testingmod.item;
+
+import com.sopa89.testingmod.item.base.ItemTM;
+import com.sopa89.testingmod.reference.Names;
+
+public class ItemGoldTag extends ItemTM
+{
+	public ItemGoldTag()
+	{
+		super();
+		setUnlocalizedName(Names.Items.GOLD_TAG);
+	}
+}
